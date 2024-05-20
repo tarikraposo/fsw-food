ef7f73461db901e1546379c05c4815cfdd7170d0
Tarik Raposo <tarikraposo@gmail.com>
2024-05-20 09:57:04 -0300

    feat: add category products page

--- app/_components/productItem.tsx
+++ app/_components/productItem.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { calcProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
 import Link from "next/link";
+import { cn } from "../_lib/utils";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -14,14 +15,18 @@ interface ProductItemProps {
       };
     };
   }>;
+  className?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, className }: ProductItemProps) => {
   return (
-    <Link className="w-[150px]" href={`/products/${product.id}`}>
+    <Link
+      className={cn("w-[150px] min-w-[150px]", className)}
+      href={`/products/${product.id}`}
+    >
       <div className="w-full space-y-2">
         {/* Image */}
-        <div className="relative h-[150px] w-[150px]">
+        <div className="relative aspect-square w-full">
           <Image
             src={product.imageUrl}
             alt={product.name}
